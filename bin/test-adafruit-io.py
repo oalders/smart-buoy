69326fb2d5abcb633045793b86da64aa043e03c2
Olaf Alders <olaf@wundersolutions.com>
2021-05-23 10:52:48 -0400

    Replace test script with a working example

--- bin/test-adafruit-io.py
+++ bin/test-adafruit-io.py
@@ -1,32 +1,3 @@
-#!/usr/bin/python3
-
-# SPDX-FileCopyrightText: 2019 Tony DiCola for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Simple demo of the MPL3115A2 sensor.
-# Will read the pressure and temperature and print them out every second.
-# import time
-# import board
-# import adafruit_mpl3115a2
-
-# Create sensor object, communicating over the board's default I2C bus
-# i2c = board.I2C()  # uses board.SCL and board.SDA
-
-# Initialize the MPL3115A2.
-# sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
-# Alternatively you can specify a different I2C address for the device:
-# sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x10)
-
-# You can configure the pressure at sealevel to get better altitude estimates.
-# This value has to be looked up from your local weather forecast or meteorological
-# reports.  It will change day by day and even hour by hour with weather
-# changes.  Remember altitude estimation from barometric pressure is not exact!
-# Set this to a value in pascals:
-# sensor.sealevel_pressure = 102250
-
-import adafruit_minimqtt.adafruit_minimqtt as MQTT
-from adafruit_io.adafruit_io import IO_MQTT
-
 # Get MQTT details and more from a secrets.py file
 try:
     from secrets import secrets
@@ -34,85 +5,16 @@ except ImportError:
     print("MQTT secrets are kept in secrets.py, please add them there!")
     raise
 
-# Define callback functions which will be called when certain events happen.
-# pylint: disable=unused-argument
-def connected(client):
-    # Connected function will be called when the client is connected to Adafruit IO.
-    # This is a good place to subscribe to feed changes.  The client parameter
-    # passed to this function is the Adafruit IO MQTT client so you can make
-    # calls against it easily.
-    print("Connected to Adafruit IO!  Listening for DemoFeed changes...")
-    # Subscribe to changes on a feed named DemoFeed.
-    client.subscribe("DemoFeed")
+# Import library and create instance of REST client.
+from Adafruit_IO import Client
+aio = Client(secrets["aio_username"], secrets["aio_key"])
 
+# Send the value 100 to a feed called 'Foo'.
+aio.send('welcome-feed', 100)
 
-def subscribe(client, userdata, topic, granted_qos):
-    # This method is called when the client subscribes to a new feed.
-    print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
-
-
-def unsubscribe(client, userdata, topic, pid):
-    # This method is called when the client unsubscribes from a feed.
-    print("Unsubscribed from {0} with PID {1}".format(topic, pid))
-
-
-# pylint: disable=unused-argument
-def disconnected(client):
-    # Disconnected function will be called when the client disconnects.
-    print("Disconnected from Adafruit IO!")
-
-
-# pylint: disable=unused-argument
-def message(client, feed_id, payload):
-    # Message function will be called when a subscribed feed has a new value.
-    # The feed_id parameter identifies the feed, and the payload parameter has
-    # the new value.
-    print("Feed {0} received new value: {1}".format(feed_id, payload))
-
-
-# Initialize MQTT interface with the ethernet interface
-MQTT.set_socket(socket, eth)
-
-# Initialize a new MQTT Client object
-mqtt_client = MQTT.MQTT(
-    broker="io.adafruit.com",
-    username=secrets["aio_username"],
-    password=secrets["aio_key"],
-)
-
-# Initialize an Adafruit IO MQTT Client
-io = IO_MQTT(mqtt_client)
-
-# Connect the callback methods defined above to Adafruit IO
-io.on_connect = connected
-io.on_disconnect = disconnected
-io.on_subscribe = subscribe
-io.on_unsubscribe = unsubscribe
-io.on_message = message
-
-# Connect to Adafruit IO
-print("Connecting to Adafruit IO...")
-io.connect()
-
-# Below is an example of manually publishing a new  value to Adafruit IO.
-last = 0
-print("Publishing a new message every 10 seconds...")
-while True:
-    # Explicitly pump the message loop.
-    io.loop()
-    # Send a new message every 10 seconds.
-    if (time.monotonic() - last) >= 5:
-        value = randint(0, 100)
-        print("Publishing {0} to DemoFeed.".format(value))
-        io.publish("DemoFeed", value)
-        last = time.monotonic()
-
-# Main loop to read the sensor values and print them.
-# while True:
-    # pressure = sensor.pressure
-    # print("Pressure: {0:0.3f} pascals".format(pressure))
-    # altitude = sensor.altitude
-    # print("Altitude: {0:0.3f} meters".format(altitude))
-    # temperature = sensor.temperature
-    # print("Temperature: {0:0.3f} degrees Celsius".format(temperature))
-    # time.sleep(5.0)
+# Retrieve the most recent value from the feed 'Foo'.
+# Access the value by reading the `value` property on the returned Data object.
+# Note that all values retrieved from IO are strings so you might need to convert
+# them to an int or numeric type if you expect a number.
+data = aio.receive('welcome-feed')
+print('Received value: {0}'.format(data.value))
